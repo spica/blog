40000baef4fd805bff195342c5356f30d8be7c04
Myunkyu Park <spica@wafflestudio.com>
2013-01-07 15:56:27 +0900

    rails project initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cd4aa077f677c469f0369db353ec5bedaedefad25fb06a963959f226cafeea8415750f29e6e10296449b024c0575b01338638e6f7a25b01457579d4bb32a9987'
